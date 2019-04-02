a535ee1e444607807f12fd8ca6d3daf86221ee2a
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-02 15:48:44 +0300

    component tree

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>
-console.log(el)
-
-//const el = React.createElement('h1', null, 'Hello world!!')
+const el = (
+  <div>
+    <h1>My Todo List</h1>
+    <input placeholder="search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+)
 
 ReactDOM.render(el, document.getElementById('root'));
