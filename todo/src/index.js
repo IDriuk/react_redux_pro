38ea0a409940e38ed3e124a96136e6beb9761b3d
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-02 15:43:28 +0300

    light weight hello world element

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = <h1>Hello World</h1>
+console.log(el)
+
+//const el = React.createElement('h1', null, 'Hello world!!')
+
+ReactDOM.render(el, document.getElementById('root'));
