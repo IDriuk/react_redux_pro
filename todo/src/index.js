22c15bf42ea43212ebb8b7bd40428ba3cffcd99c
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-03 09:40:17 +0300

    array and spread in props

--- todo/src/index.js
+++ todo/src/index.js
@@ -6,13 +6,18 @@ import SearchPanel from './components/search-panel'
 import TodoList from './components/todo-list'
 
 const App = () => {
-  const value ='<script>alert("")</script>'
+  
+  const todoData = [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Make Awesome App', important: true},
+    {label: 'Have a lunch', important: false},
+  ];
+
   return (
     <div>
-      { value }
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
     </div>
   )
 }
