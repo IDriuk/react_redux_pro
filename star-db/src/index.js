635ba98e6781f78fcd8a9c60823ad032bdf14685
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-04 16:24:56 +0300

    fetch api, async await, promise

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const getResouce = async (url) => {
+  const res = await fetch(url)
+  const body = await res.json()
+  return body
+}
+
+getResouce('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log(body)
+  })
+
+// fetch('https://swapi.co/api/people/1/')
+//   .then((res) => {
+//     return res.json()
+//   })
+//   .then((body) => {
+//     console.log(body)
+//   })
