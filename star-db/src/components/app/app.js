d1e311a372a4572806a3ca5e67d23fe43f5b196f
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-05 15:50:15 +0300

    people page with error boundaries

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
 import ErrorButton from '../error-button';
-import ErrorIndicator from '../error-indicator'
+import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
@@ -13,7 +12,6 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 5,
     hasError: false
   };
 
@@ -25,15 +23,8 @@ export default class App extends Component {
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
-
   componentDidCatch() {
-    console.log('componentDidCatch()')
-    this.setState({hasError: true})
+    this.setState({ hasError: true });
   }
 
   render() {
@@ -60,14 +51,10 @@ export default class App extends Component {
           <ErrorButton />
         </div>
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.onPersonSelected}/>
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
+        <PeoplePage />
+        <PeoplePage />
+        <PeoplePage />
+
       </div>
     );
   }
