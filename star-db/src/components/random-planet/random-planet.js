ca95b83c5fbedeeeac3824a584a213b68e222c7b
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-05 14:35:16 +0300

    componentWillUnmount

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -22,14 +22,12 @@ export default class RandomPlanet extends Component {
   componentDidMount() {
     this.updatePlanet()
     this.interval = setInterval(this.updatePlanet, 2500)
-    // clearInterval(this.interval)
-  }
-
-  componentDidUpdate() {
   }
 
   componentWillUnmount() {
+    clearInterval(this.interval)
   }
+
 
   onPlanetLoaded = (planet) => {
     this.setState({ planet, loading: false })
