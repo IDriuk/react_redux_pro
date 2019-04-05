1dcbf141014bbf45e375bd7326b3a81121fed146
Ivan Driuk <idriuk@zaraffasoft.com>
2019-04-05 12:19:10 +0300

    update planet with setInterval

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -18,6 +18,9 @@ export default class RandomPlanet extends Component {
   constructor() {
     super()
     this.updatePlanet()
+    this.interval = setInterval(this.updatePlanet, 2500)
+
+    // clearInterval(this.interval)
   }
 
   onPlanetLoaded = (planet) => {
@@ -31,8 +34,9 @@ export default class RandomPlanet extends Component {
     })
   }
 
-  updatePlanet() {
-    const id = 15//Math.floor(Math.random()*25) + 2
+  updatePlanet = () => {
+    console.log('update')
+    const id = Math.floor(Math.random()*25) + 3
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
